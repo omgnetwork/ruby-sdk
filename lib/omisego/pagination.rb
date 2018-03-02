d92b7b0629c2fd05b324d0319d2ecaf6306e5c92
Joe Woodward <j@joewoodward.me>
2018-03-02 14:05:10 +0700

    set methods to private rather than overriding

--- lib/omisego/pagination.rb
+++ lib/omisego/pagination.rb
@@ -1,6 +1,7 @@
 module OmiseGO
   class Pagination < Base
     attributes :per_page, :is_last_page, :is_first_page, :current_page
+    private :is_first_page, :is_last_page
 
     def first_page?
       is_first_page
@@ -8,16 +9,6 @@ module OmiseGO
 
     def last_page?
       is_last_page
-    end
-
-    private
-
-    def is_first_page
-      @is_first_page
-    end
-
-    def is_last_page
-      @is_last_page
     end
   end
 end
