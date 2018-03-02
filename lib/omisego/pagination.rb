0706e841947eb5e16405e6747a11c65c3da3a219
Joe Woodward <j@joewoodward.me>
2018-03-02 12:57:05 +0700

    Update the pagination methods
    `is_first_page` => `first_page?`
    `is_last_page` => `last_page?`
    
    Follows ruby-style-guide
    https://github.com/bbatsov/ruby-style-guide\#bool-methods-qmark
    https://github.com/bbatsov/ruby-style-guide\#bool-methods-prefix

--- lib/omisego/pagination.rb
+++ lib/omisego/pagination.rb
@@ -1,5 +1,23 @@
 module OmiseGO
   class Pagination < Base
     attributes :per_page, :is_last_page, :is_first_page, :current_page
+
+    def first_page?
+      is_first_page
+    end
+
+    def last_page?
+      is_last_page
+    end
+
+    private
+
+    def is_first_page
+      @is_first_page
+    end
+
+    def is_last_page
+      @is_last_page
+    end
   end
 end
